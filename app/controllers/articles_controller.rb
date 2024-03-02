d679eea9242ff1f26609cb2b88fa162a76ea5494
Vikash   Gupta <vikash.gupta@unicosmosoft.com>
2024-03-02 17:34:11 +0530

    ubloading blog application

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController <ApplicationController
+    def index
+        @articles = Article.all
+    end
+    def new 
+        @article = Article.new
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def create
+        @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    def Edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+       if @article.update(article_params)
+        redirect_to @article
+       else
+        render :new, status: :unprocessable_entity
+       end
+    end
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
