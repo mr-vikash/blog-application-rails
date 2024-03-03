b593979fe6ed71c14256038ade38f03774e4f18f
Vikash   Gupta <vikash.gupta@unicosmosoft.com>
2024-03-03 11:24:33 +0530

    optimizing the code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,11 +29,13 @@ class ArticlesController <ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+  
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to root_path, status: :see_other
   end
+  
     private
     def article_params
       params.require(:article).permit(:title, :body)
